3ea3e74f631ab112424d8477c2e864afcf93e1f9
mellzamora <mellzamora@outlook.com>
2015-04-21 11:48:23 +0800

    refactored deprecated OptionsResolverInterface to OptionsResolver

--- Form/Type/CategoryChoiceType.php
+++ Form/Type/CategoryChoiceType.php
@@ -3,9 +3,9 @@
 namespace Rz\NewsBundle\Form\Type;
 
 use Symfony\Component\Form\Extension\Core\ChoiceList\SimpleChoiceList;
-use Rz\NewsBundle\Form\Type\TreeList\TreeList;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Sonata\NewsBundle\Model\CategoryManagerInterface;
 use Symfony\Component\Form\FormInterface;
@@ -32,35 +32,6 @@ class CategoryChoiceType extends AbstractType
     }
 
     /**
-     * {@inheritDoc}
-     */
-    public function getParent()
-    {
-        return 'sonata_type_model';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $that = $this;
-
-        $resolver->setDefaults(
-            array(
-               'expanded' => true,
-               'current' => null,
-               'tree_enabled' => true,
-//               'nested_tree' => function (Options $opts, $previousValue) use ($that) {
-//                   return $that->manager->fetchCategoriesTree();
-//               },
-               'choice_list' => function (Options $opts, $previousValue) use ($that) {
-                   return new SimpleChoiceList($that->getChoices($opts));
-               },
-           ));
-    }
-
-    /**
      * @param Options $options
      *
      * @return array
@@ -73,6 +44,41 @@ class CategoryChoiceType extends AbstractType
             $choices[$category->getId()] = $category;
         }
         return $choices;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @todo Remove it when bumping requirements to SF 2.7+
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $this->configureOptions($resolver);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $that = $this;
+        $resolver->setDefaults(
+            array(
+                'expanded' => true,
+                'current' => null,
+                'tree_enabled' => true,
+                'choice_list' => function (Options $opts, $previousValue) use ($that) {
+                    return new SimpleChoiceList($that->getChoices($opts));
+                },
+            ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent()
+    {
+        return 'sonata_type_model';
     }
 
     /**
